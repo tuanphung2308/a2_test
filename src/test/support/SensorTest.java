d7f41e8a7d685758733d8ec67696dcdd9b73a822
tuanphung2308@gmail.com <tuan2308>
2019-09-05 11:58:16 +0700

    hashmap test

--- src/test/support/SensorTest.java
+++ src/test/support/SensorTest.java
@@ -20,22 +20,26 @@ public class SensorTest {
     private String sensorType;
     private LinkedHashMap linkedHashMap;
 
-//    public SensorTest(String p1, String p2, LinkedHashMap p3) {
-//        sensorUsername = p1;
-//        sensorType = p2;
-//        p3 = linkedHashMap;
-//    }
-
-    public SensorTest(String p1, String p2) {
+    public SensorTest(String p1, String p2, LinkedHashMap p3) {
         sensorUsername = p1;
         sensorType = p2;
+        linkedHashMap = p3;
     }
+
     // creates the test data
     @Parameters
     public static Collection<Object[]> data() {
+        LinkedHashMap m1 = new LinkedHashMap();
+        LinkedHashMap m2 = new LinkedHashMap();
+        m1.put("10", 5);
+        m1.put("15", 3);
+        m1.put("20", 4);
+
+        m2.put("200", 15);
+        m2.put("90", 11);
         Object[][] data = new Object[][] {
-                { "Jack" , "Temperature" , },
-                { "David", "AQI" , },
+                { "Jack" , "Temperature" , m1},
+                { "David", "AQI" , m2},
         };
         return Arrays.asList(data);
     }
@@ -58,7 +62,8 @@ public class SensorTest {
         try {
             Method method = sensor.getClass().getDeclaredMethod("readData");
             method.setAccessible(true);
-            Object res = method.invoke(sensor);
+            LinkedHashMap res = (LinkedHashMap) method.invoke(sensor);
+            assertEquals("Equal", res, linkedHashMap);
             System.out.println(res);
         } catch(NoSuchMethodException e) {
         } catch(IllegalAccessException e) {
