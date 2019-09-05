c04665d3cde16b68c3f327f182c096eba2dfa975
tuanphung2308@gmail.com <tuan2308>
2019-09-05 14:40:37 +0700

    - PrefRepo Init Test

---
+++ src/test/main/preference/PrefWorker.java
@@ -0,0 +1,14 @@
+package test.main.preference;
+
+import com.zeroc.Ice.Current;
+import helper.PreferenceRequest;
+import helper.SensorData;
+import helper.User;
+import support.Preference;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrefWorker {
+}
