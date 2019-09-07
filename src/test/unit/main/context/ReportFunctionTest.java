ee45431e72ed7fe22d1f45edb76c9e9f80253aa4
tuanphung2308@gmail.com <tuan2308>
2019-09-08 00:21:13 +0700

    Finalize test case

--- src/test/unit/main/context/ReportFunctionTest.java
+++ src/test/unit/main/context/ReportFunctionTest.java
@@ -43,6 +43,8 @@ public class ReportFunctionTest {
     public boolean apoReached;
     public boolean tempReached;
 
+    private SensorData sensorData = new SensorData();
+
     public ReportFunctionTest(int[] tempThreshholds, int medicalCondition, int currentWeather, int currentClock, String previouslocationStatus, String currentLocationStatus, int previousaqi, int previousTemperature, int currentaqi, int currentTemperature, int expectedClock, boolean expectedApoReached, boolean expectedTempReached) {
         this.tempThreshholds = tempThreshholds;
         this.medicalCondition = medicalCondition;
@@ -62,27 +64,114 @@ public class ReportFunctionTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         Object[][] data = new Object[][] {
-                {new int[]{25, 30}, 1, 0 , 0, INDOOR, INDOOR, 0, 20, 0, 20, 0, false, false},
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 0, // weather, clock
+                        INDOOR, INDOOR,  //previous, current loc,
+                        0, 20, // previous aqi, temp
+                        0, 20, // current aqi, temp
+                        0, //expected clock
+                        false, false}, //expected apo, temp reached
+                {new int[]{20, 30}, 1,
+                        0 , 0,
+                        INDOOR, INDOOR, 0,
+                        20, 0,
+                        20, 0,
+                        false, true},
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 0, // weather, clock
+                        INDOOR, INDOOR,  //previous, current loc,
+                        0, 20, // previous aqi, temp
+                        0, 26, // current aqi, temp
+                        0, //expected clock
+                        false, true}, //expected apo, temp reached
+                {new int[]{25, 31}, 1, // temp thresh, med cond
+                        0 , 0, // weather, clock
+                        INDOOR, INDOOR,  //previous, current loc,
+                        0, 20, // previous aqi, temp
+                        0, 26, // current aqi, temp
+                        0, //expected clock
+                        false, true}, //expected apo, temp reached
+                //OUTDOOR - OUTDOOR only apo reached
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 10, // weather, clock
+                        OUTDOOR, OUTDOOR,  //previous, current loc,
+                        50, 20, // previous aqi, temp
+                        50, 24, // current aqi, temp
+                        11, //expected clock
+                        false, false}, //expected apo, temp reached
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 29, // weather, clock
+                        OUTDOOR, OUTDOOR,  //previous, current loc,
+                        50, 20, // previous aqi, temp
+                        50, 24, // current aqi, temp
+                        30, //expected clock
+                        true, false}, //expected apo, temp reached
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 30, // weather, clock
+                        OUTDOOR, OUTDOOR,  //previous, current loc,
+                        50, 20, // previous aqi, temp
+                        50, 24, // current aqi, temp
+                        30, //expected clock // should stop once it reached the threshhold
+                        true, false}, //expected apo, temp reached
+                //INDOOR - OUTDOOR only apo reached
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 0, // weather, clock
+                        INDOOR, OUTDOOR,  //previous, current loc,
+                        50, 20, // previous aqi, temp
+                        50, 24, // current aqi, temp
+                        1, //expected clock
+                        false, false},
+                //OUTDOOR - INDOOR only apo reached
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 10, // weather, clock
+                        OUTDOOR, INDOOR,  //previous, current loc,
+                        50, 20, // previous aqi, temp
+                        50, 24, // current aqi, temp
+                        0, //expected clock
+                        false, false}, //expected apo, temp reached
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 29, // weather, clock
+                        OUTDOOR, INDOOR,  //previous, current loc,
+                        50, 20, // previous aqi, temp
+                        50, 24, // current aqi, temp
+                        0, //expected clock
+                        false, false}, //expected apo, temp reached
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 30, // weather, clock
+                        OUTDOOR, INDOOR,  //previous, current loc,
+                        50, 20, // previous aqi, temp
+                        50, 24, // current aqi, temp
+                        0, //expected clock // should stop once it reached the threshhold
+                        false, false}, //expected apo, temp reached
+                //INDOOR - INDOOR only apo reached
+                {new int[]{25, 30}, 1, // temp thresh, med cond
+                        0 , 0, // weather, clock
+                        INDOOR, INDOOR,  //previous, current loc,
+                        50, 20, // previous aqi, temp
+                        50, 24, // current aqi, temp
+                        0, //expected clock
+                        false, false}, //expected apo, temp reached
         };
         return Arrays.asList(data);
+    }
+    @Before
+    public void before() throws Exception{
+        this.sensorData = new SensorData();
+        this.sensorData.temperature = currentTemperature;
+        this.sensorData.aqi = currentaqi;
+
+        user.medicalConditionType = this.medicalCondition;
+        user.tempThreshholds = this.tempThreshholds;
+        user.sensorData = this.sensorData;
+        user.apoThreshhold = calculateapoThreshhold(user);
     }
 
     @Test
     public void testReport() throws Exception{
-        SensorData sensorData = new SensorData();
-        sensorData.temperature = currentTemperature;
-        sensorData.aqi = currentaqi;
-        report(sensorData, null);
+        report(this.sensorData, null);
         assertEquals("Clock should be equal", expectedClock, currentClock);
         assertEquals("apoReached?", expectedApoReached, apoReached);
         assertEquals("tempReached?", expectedTempReached, tempReached);
-    }
-
-    @Before
-    public void before() throws Exception{
-        user.medicalConditionType = this.medicalCondition;
-        user.tempThreshholds = this.tempThreshholds;
-        user.apoThreshhold = calculateapoThreshhold(user);
     }
 
     private static Integer calculateapoThreshhold(User user) throws Exception{
@@ -92,8 +181,8 @@ public class ReportFunctionTest {
         return result;
     }
 
-    private static boolean checkapoReached(User user) {
-        return user.clock == user.apoThreshhold;
+    private boolean checkapoReached(User user) {
+        return this.currentClock == user.apoThreshhold;
     }
 
     private static boolean checkTempReached(User user) {
